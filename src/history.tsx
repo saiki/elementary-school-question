6ce13dbc3078ac49ec136f03a365afbef4445bac
saiki <akio.saiki@gmail.com>
2019-07-17 00:52:38 +0900

    add close button.

--- src/history.tsx
+++ src/history.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 import { BlankPosition, FillQuizFormulaValue } from './fillQuiz';
 
 export type History = FillQuizFormulaValue & {
@@ -10,34 +12,82 @@ type HistoryDialogProps = {
 	history: History[];
 }
 
+
+const JapaneseFontLabel = styled.span`
+	font-family: "misakigothic2nd";
+	font-size: large;
+`;
+
+const BlankInput = styled.input`
+	width: auto;
+`;
+
 const HistoryList: React.FunctionComponent<HistoryDialogProps> = (props: HistoryDialogProps) => {
 	let list = props.history.map((h:History) => {
-		const blank = <span className="nes-badge"><span className="is-dark">&nbsp;</span></span>;
+		const blank = <BlankInput type="text" className="nes-input" />;
 		let left = <span>{h.left}</span>;
 		let operator = <span>{h.operator}</span>;
 		let right = <span>{h.right}</span>;
 		let answer = <span>{h.answer}</span>;
+		let actual;
 		switch (h.blankPos) {
 			case BlankPosition.Left:
+				actual = left;
 				left = blank;
 				break;
 			case BlankPosition.Operator:
+				actual = operator;
 				operator = blank;
 				break;
 			case BlankPosition.Right:
+				actual = right;
 				right = blank;
 				break;
 			case BlankPosition.Answer:
+				actual = answer;
 				answer = blank;
 				break;
 		}
-		return (<li>{left}{operator}{right}={answer}&nbsp;こたえ&nbsp;{h.input}</li>);
+		let jadge;
+		if (h.correct) {
+			jadge = "○";
+		} else {
+			jadge = "×";
+		}
+		return (
+			<tr>
+				<td><JapaneseFontLabel>{jadge}</JapaneseFontLabel></td>
+				<td>{left}{operator}{right}={answer}</td>
+				<td>{h.input}</td>
+				<td>{actual}</td>
+			</tr>
+		);
 	});
 	return (
-		<div className="nes-container with-title is-centered">
-			<ul className="nes-list is-disc">
-				{list}
-			</ul>
+		<div className="nes-container is-rounded">
+			<header className="sticky">
+				<div className="container">
+					<div className="nav-brand"></div>
+					<div>
+						<Link to="/"><button className="nes-btn"><i className="nes-icon close is-small"></i></button></Link>
+					</div>
+				</div>
+			</header>
+			<div className="nes-table-responsive">
+				<table className="nes-table is-bordered is-centered">
+					<thead>
+						<tr>
+							<th><JapaneseFontLabel>○×</JapaneseFontLabel></th>
+							<th><JapaneseFontLabel>しき</JapaneseFontLabel></th>
+							<th><JapaneseFontLabel>こたえ</JapaneseFontLabel></th>
+							<th><JapaneseFontLabel>せいかい</JapaneseFontLabel></th>
+						</tr>
+					</thead>
+					<tbody>
+						{list}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
