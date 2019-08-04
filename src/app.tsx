14c6b29bf50938cef89758fa488a4a1741efe5fe
saiki <akio.saiki@gmail.com>
2019-08-04 16:54:10 +0900

    add sound toggle.

--- src/app.tsx
+++ src/app.tsx
@@ -4,16 +4,25 @@ import { FillQuiz, FillQuizFormulaValue } from './fillQuiz';
 import Header from './header';
 import HistoryList, {History} from './history';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { SoundSetting, AnswerSound } from './sound';
 
 type AppState = {
 	history: History[];
+	soundSetting: SoundSetting;
 }
 
 export class App extends React.Component<any, AppState> {
+
+	private soundRef = React.createRef<AnswerSound>();
+
 	constructor(props: any) {
 		super(props);
-		this.state = { history: [] };
+		this.state = {
+			history: [],
+			soundSetting: SoundSetting.Mute
+		};
 		this.onResult = this.onResult.bind(this);
+		this.onSoundSettingChange = this.onSoundSettingChange.bind(this);
 	}
 
 	onResult(correct: boolean, userInput: string, quiz: FillQuizFormulaValue) {
@@ -28,19 +37,34 @@ export class App extends React.Component<any, AppState> {
 				blankPos: quiz.blankPos
 			})
 		});
+		if (this.soundRef && this.soundRef.current) {
+			if (correct) {
+				this.soundRef.current.correct();
+			} else {
+				this.soundRef.current.wrong();
+			}
+		}
+	}
+
+	onSoundSettingChange() {
+		let setting = this.state.soundSetting;
+		setting = setting + 1;
+		setting = setting % 3;
+		this.setState({soundSetting: setting});
 	}
 
 	render() {
 		let correctCount = this.state.history.filter((h:History) => {return h.correct}).length;
 		return (
 			<Router>
-				<Header correctCount={correctCount} totalCount={this.state.history.length}/>
+				<Header correctCount={correctCount} totalCount={this.state.history.length} soundSetting={this.state.soundSetting} onSoundSettingClick={this.onSoundSettingChange} />
 				<div className="container">
 					<main className="main-content">
 						<Route exact path="/" render={() => <FillQuiz onResult={this.onResult} key={this.state.history.length} />} />
 						<Route path="/history" render={(props) => { return (<HistoryList history={this.state.history} />) }} />
 					</main>
 				</div>
+				<AnswerSound setting={this.state.soundSetting} ref={this.soundRef} />
 			</Router>
 		);
 	}
--- src/header.tsx
+++ src/header.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { SoundToggle, SoundSetting } from './sound';
 
 type HeaderProps = {
 	correctCount: number;
 	totalCount: number;
+	soundSetting: SoundSetting;
+	onSoundSettingClick: () => any;
 }
 
 const Header: React.FunctionComponent<HeaderProps> = (props:HeaderProps) => {
+
 	return (
 		<header className="sticky">
 			<div className="container">
 				<div className="nav-brand">
 					<i className="snes-jp-logo brand-logo"></i>
+					<SoundToggle setting={props.soundSetting} onClick={props.onSoundSettingClick} />
 				</div>
 				<div>
 					<Link to="/history">
